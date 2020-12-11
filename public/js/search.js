d93486770d34af134c8deae9d09487e4050a6b82
Alex Nordin <apnordin@gmail.com>
2020-12-11 13:07:43 -0500

    TestIndex with working classes on search

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,6 @@
 // External API- IMDB Search, and setting newMovie object
 movieSearch = () => {
-  const searchInput = "";
+  const searchInput = "Avengers Endgame";
 
   queryURL = "http://www.omdbapi.com/?t=" + searchInput + "&apikey=6c279cf4";
 
@@ -19,17 +19,18 @@ movieSearch = () => {
       genre: response.Genre
     }
   });
-}
+};
 
 movieSearch();
 
-// // Code for future use when populating divs
-// movieSelect.click(() => {
-//   $(".movie-name").append(newMovie.name);
-//   $(".poster-image").attr("src", newMovie.poster);
-//   // Make sure to use this code in HTML: <img class="images" height="300" src="">
-//   $(".synopsis").append(newMovie.synopsis);
-//   $(".rating").append(newMovie.rating);
-//   $(".release-date").append(newMovie.releaseDate);
-//   $(".genre").append(newMovie.genre);
-// });
+// Code for future use when populating divs
+$("#movieSelect").click(() => {
+  console.log("clicked");
+  $(".movie-name").append(newMovie.name);
+  $(".poster-image").attr("src", newMovie.poster);
+  // Make sure to use this code in HTML:<img class="poster-image" height="300" src="">
+  $(".synopsis").append(newMovie.synopsis);
+  $(".rating").append(newMovie.rating);
+  $(".release-date").append(newMovie.releaseDate);
+  $(".genre").append(newMovie.genre);
+});

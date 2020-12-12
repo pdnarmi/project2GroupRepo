cabc5452db776eb263e3d89bd3bad7f0d29f04f5
Alex Nordin <apnordin@gmail.com>
2020-12-12 00:20:31 -0500

    On running server.js, sequelize adds movies table

---
+++ models/movies.js
@@ -0,0 +1,11 @@
+module.exports = function(sequelize, DataTypes) {
+  const Movie = sequelize.define("Movie", {
+    title: DataTypes.STRING,
+    poster: DataTypes.STRING,
+    synopsis: DataTypes.STRING,
+    rating: DataTypes.STRING,
+    releaseDate: DataTypes.STRING,
+    genre: DataTypes.STRING
+  });
+  return Movie;
+};

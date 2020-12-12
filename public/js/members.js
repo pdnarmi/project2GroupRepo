447f5422a8858ae5876f9e8173e9d32d6b2f7036
GBondarek <georgettebondarek@gmail.com>
2020-12-12 13:47:51 -0500

    navbar

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,3 @@
-$(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
-  });
+$(document).ready(function(){
+  $('.sidenav').sidenav();
 });

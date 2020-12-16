b6e7e6be96f491f5c38e9c5166a57eb3be7b25dd
Alex Nordin <apnordin@gmail.com>
2020-12-15 21:57:26 -0500

    first name and last name show up in welcome

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.firstName);
+    const userFullName = data.firstName + " " + data.lastName;
+    $(".member-name").text(userFullName);
   });
 });
